cc0dc05eb50a012bc32ef2043f158f0b09d2dde1
Tom Ellis <tellishtc@gmail.com>
2014-08-15 19:58:26 +0100

    Remove old Bundle generator code

--- src/Tomahawk/Bundle/GeneratorBundle/Generator/BundleGenerator.php
+++ src/Tomahawk/Bundle/GeneratorBundle/Generator/BundleGenerator.php
@@ -36,7 +36,7 @@ class BundleGenerator extends Generator
         $this->filesystem = $filesystem;
     }
 
-    public function generate($namespace, $bundle, $dir/*, $format, $structure*/)
+    public function generate($namespace, $bundle, $dir)
     {
         $dir .= '/'.strtr($namespace, '\\', '/');
 
@@ -57,37 +57,9 @@ class BundleGenerator extends Generator
         $parameters = array(
             'namespace' => $namespace,
             'bundle'    => $bundle,
-            //'format'    => $format,
             'bundle_basename' => $basename,
         );
 
         $this->renderFile('bundle/Bundle.php.twig', $dir.'/'.$bundle.'.php', $parameters);
-        //$this->renderFile('bundle/Extension.php.twig', $dir.'/DependencyInjection/'.$basename.'Extension.php', $parameters);
-        //$this->renderFile('bundle/Configuration.php.twig', $dir.'/DependencyInjection/Configuration.php', $parameters);
-        //$this->renderFile('bundle/DefaultController.php.twig', $dir.'/Controller/DefaultController.php', $parameters);
-        //$this->renderFile('bundle/DefaultControllerTest.php.twig', $dir.'/Tests/Controller/DefaultControllerTest.php', $parameters);
-        //$this->renderFile('bundle/index.html.twig.twig', $dir.'/Resources/views/Default/index.html.twig', $parameters);
-
-        /*
-        if ('xml' === $format || 'annotation' === $format) {
-            $this->renderFile('bundle/services.xml.twig', $dir.'/Resources/config/services.xml', $parameters);
-        } else {
-            $this->renderFile('bundle/services.'.$format.'.twig', $dir.'/Resources/config/services.'.$format, $parameters);
-        }
-
-        if ('annotation' != $format) {
-            $this->renderFile('bundle/routing.'.$format.'.twig', $dir.'/Resources/config/routing.'.$format, $parameters);
-        }
-
-        if ($structure) {
-            $this->renderFile('bundle/messages.fr.xlf', $dir.'/Resources/translations/messages.fr.xlf', $parameters);
-
-            $this->filesystem->mkdir($dir.'/Resources/doc');
-            $this->filesystem->touch($dir.'/Resources/doc/index.rst');
-            $this->filesystem->mkdir($dir.'/Resources/translations');
-            $this->filesystem->mkdir($dir.'/Resources/public/css');
-            $this->filesystem->mkdir($dir.'/Resources/public/images');
-            $this->filesystem->mkdir($dir.'/Resources/public/js');
-        }*/
     }
 }
