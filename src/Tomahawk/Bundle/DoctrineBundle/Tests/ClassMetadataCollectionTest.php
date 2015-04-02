faf462299f1b2b2282fe0b43c0282095ae89a8dd
Tom Ellis <tellishtc@gmail.com>
2015-04-02 08:31:06 +0100

    Add new test for ClassMetadataCollection

---
+++ src/Tomahawk/Bundle/DoctrineBundle/Tests/ClassMetadataCollectionTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tomahawk\Bundle\DoctrineBundle\Tests;
+
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+use Tomahawk\Bundle\DoctrineBundle\Mapping\ClassMetadataCollection;
+use Tomahawk\Test\TestCase;
+
+class ClassMetadataCollectionTest extends TestCase
+{
+    public function testGetMetadata()
+    {
+        $class = new ClassMetadataInfo(__CLASS__);
+        $collection = new ClassMetadataCollection(array($class));
+
+        $this->assertEquals(array($class), $collection->getMetadata());
+    }
+
+    public function testSetGetPath()
+    {
+        $class = new ClassMetadataInfo(__CLASS__);
+        $collection = new ClassMetadataCollection(array($class));
+        $collection->setPath('/path');
+
+        $this->assertEquals('/path', $collection->getPath());
+    }
+
+    public function testSetGetNamespace()
+    {
+        $class = new ClassMetadataInfo(__CLASS__);
+        $collection = new ClassMetadataCollection(array($class));
+        $collection->setNamespace('MyBundle');
+
+        $this->assertEquals('MyBundle', $collection->getNamespace());
+    }
+}
