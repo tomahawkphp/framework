eaf1ff5498e5c50229ea684511cac88055f1d4ec
Tom Ellis <tellishtc@gmail.com>
2015-06-12 18:25:53 +0100

    Update kernel to 1.2.1

--- src/Tomahawk/HttpKernel/Kernel.php
+++ src/Tomahawk/HttpKernel/Kernel.php
@@ -41,11 +41,11 @@ abstract class Kernel implements KernelInterface, TerminableInterface
     protected $paths = array();
     protected $routePaths = array();
 
-    const VERSION         = '1.2.0';
-    const VERSION_ID      = '10200';
+    const VERSION         = '1.2.1';
+    const VERSION_ID      = '10201';
     const MAJOR_VERSION   = '1';
     const MINOR_VERSION   = '2';
-    const RELEASE_VERSION = '0';
+    const RELEASE_VERSION = '1';
     const EXTRA_VERSION   = '';
 
     /**
