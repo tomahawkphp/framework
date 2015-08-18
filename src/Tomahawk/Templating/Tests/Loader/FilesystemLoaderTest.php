9b2e17a72b77c61e01fba76ab7c22fa68527d65d
Tom Ellis <tellishtc@gmail.com>
2015-08-18 16:43:49 +0100

    Update template filesystemloader test

--- src/Tomahawk/Templating/Tests/Loader/FilesystemLoaderTest.php
+++ src/Tomahawk/Templating/Tests/Loader/FilesystemLoaderTest.php
@@ -42,7 +42,6 @@ class FilesystemLoaderTest extends PHPUnit_Framework_TestCase
     public function testFreshReturnFalseOnFailure()
     {
         $reference = new TemplateReference(null, null, 'index', 'php');
-        $fileStorage = new FileStorage(__DIR__ .'/Resources/index.php');
 
         $locator = $this->getMock('Symfony\Component\Config\FileLocatorInterface', array('locate'));
         $locator->expects($this->any())
@@ -71,6 +70,9 @@ class FilesystemLoaderTest extends PHPUnit_Framework_TestCase
 
     public function testIsFresh()
     {
+        $filesystem = new Filesystem();
+        $filesystem->touch(__DIR__ .'/Resources/index.php');
+
         $reference = new TemplateReference(null, null, 'index', 'php');
         $fileStorage = new FileStorage(__DIR__ .'/Resources/index.php');
 
@@ -83,5 +85,4 @@ class FilesystemLoaderTest extends PHPUnit_Framework_TestCase
 
         $this->assertTrue($filesystemLoader->isFresh($reference, time() + 3600));
     }
-
 }
