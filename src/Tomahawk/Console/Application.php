616ee66aebca754aa2d117028d1005d210aa8996
Tom Ellis <tellishtc@gmail.com>
2016-08-24 12:51:20 +0100

    Remove shell from console application

--- src/Tomahawk/Console/Application.php
+++ src/Tomahawk/Console/Application.php
@@ -14,7 +14,6 @@ namespace Tomahawk\Console;
 use Tomahawk\HttpKernel\KernelInterface;
 use Tomahawk\HttpKernel\Bundle\Bundle;
 use Tomahawk\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\Console\Shell;
 use Symfony\Component\Console\Application as BaseApplication;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -44,7 +43,6 @@ class Application extends BaseApplication
 
         parent::__construct('Tomahawk', Kernel::VERSION.' - '.$kernel->getName().'/'.$kernel->getEnvironment().($kernel->isDebug() ? '/debug' : ''));
 
-        $this->getDefinition()->addOption(new InputOption('--shell', '-s', InputOption::VALUE_NONE, 'Launch the shell.'));
         $this->getDefinition()->addOption(new InputOption('--process-isolation', null, InputOption::VALUE_NONE, 'Launch commands from shell as a separate process.'));
         $this->getDefinition()->addOption(new InputOption('--env', '-e', InputOption::VALUE_REQUIRED, 'The Environment name.', $kernel->getEnvironment()));
         $this->getDefinition()->addOption(new InputOption('--no-debug', null, InputOption::VALUE_NONE, 'Switches off debug mode.'));
@@ -85,18 +83,7 @@ class Application extends BaseApplication
 
         $this->setDispatcher($container->get('event_dispatcher'));
 
-        // @codeCoverageIgnoreStart
-        if (true === $input->hasParameterOption(array('--shell', '-s'))) {
-
-            $shell = new Shell($this);
-            $shell->setProcessIsolation($input->hasParameterOption(array('--process-isolation')));
-            $shell->run();
-
-            return 0;
-        }
-
         return parent::doRun($input, $output);
-        // @codeCoverageIgnoreEnd
     }
 
     /**
