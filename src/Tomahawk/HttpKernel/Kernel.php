cee823982c89039a6e956f5889f20f9fddddff6d
Tom Ellis <tellishtc@gmail.com>
2017-05-20 18:14:54 +0100

    Update kernel to beta 3 [skip ci]

--- src/Tomahawk/HttpKernel/Kernel.php
+++ src/Tomahawk/HttpKernel/Kernel.php
@@ -82,7 +82,7 @@ abstract class Kernel implements KernelInterface, TerminableInterface
     const MAJOR_VERSION   = '2';
     const MINOR_VERSION   = '1';
     const RELEASE_VERSION = '0';
-    const EXTRA_VERSION   = 'b2';
+    const EXTRA_VERSION   = 'b3';
 
     /**
      * Constructor.
